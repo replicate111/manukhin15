03d376313a1c1525a6f4d29b5e160f4981badaea
replicate <replicate@DESKTOP-J7M1L13>
2022-05-22 16:04:30 +0300

    Добавьте файлы проекта.

---
+++ manukhin15/manukhin15/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using manukhin15.Models;
+using manukhin15.Views;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace manukhin15
+{
+    public partial class App : Application
+    {
+
+        static TimeDataBase dataBase;
+
+        // Create the database connection as a singleton.
+        internal static TimeDataBase DataBase
+        {
+            get
+            {
+                if (dataBase == null)
+                {
+                    dataBase = new TimeDataBase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Times.db3"));
+                }
+                return dataBase;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ manukhin15/manukhin15/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using manukhin15.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace manukhin15
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
